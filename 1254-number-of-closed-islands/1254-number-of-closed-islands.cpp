cb2ed53aa1f8778b6e24e767b3a7aef3c692b4ba
Md Ehteshmaul Kabir <61960655+ehteshampantho@users.noreply.github.com>
2024-02-23 19:39:03 +0600

    Time: 8 ms (82.69%), Space: 12.7 MB (32.10%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,48 +1,47 @@
 class Solution {
 public:
     
-    int row;
-    int col;
-    bool vis[105][105];
-    
+    bool vis[100][100];
     vector<pair<int,int>> d = {{0,1},{0,-1},{1,0},{-1,0}};
     
-    bool valid(int ci, int cj){
-        if(ci<0 || ci>=row || cj<0 || cj>=col) return false;
+    int n,m;
+    
+    bool valid(int ci,int cj){
+        if(ci<0 || ci>=n || cj<0 || cj>=m) return false;
         else return true;
     }
     
     bool flag;
     
-    void dfs(int si, int sj, vector<vector<int>>& grid){
+    void dfs(int si,int sj,vector<vector<int>>& grid){
         vis[si][sj] = true;
-        if(si == 0 || si == row-1 || sj == 0 || sj == col-1) flag = false;
+        if(si==0 || si==n-1 || sj==0 || sj==m-1) flag = false;
         
         for(int i=0;i<4;i++){
             int ci = si + d[i].first;
             int cj = sj + d[i].second;
-            if(valid(ci,cj)==true && vis[ci][cj] == false && grid[ci][cj] == 0){
-                dfs(ci,cj, grid);
+            if(valid(ci,cj) && !vis[ci][cj] && grid[ci][cj]==0){
+                dfs(ci,cj,grid);
             }
         }
     }
     
     int closedIsland(vector<vector<int>>& grid) {
-        row = grid.size();
-        col = grid[0].size();
-        int cnt = 0;
+        n = grid.size();
+        m = grid[0].size();
+        int ans = 0;
         
         memset(vis,false,sizeof(vis));
         
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                if(vis[i][j] == false && grid[i][j] == 0){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!vis[i][j] && grid[i][j]==0){
                     flag = true;
-                    dfs(i,j, grid);
-                    if(flag) cnt++;
+                    dfs(i,j,grid);
+                    if(flag) ans++;
                 }
             }
         }
-        return cnt;
+        return ans;
     }
 };
