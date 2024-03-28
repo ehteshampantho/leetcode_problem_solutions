5fde43b5b77f54b58c91ca81bf81d2c6bbfefc34
Md Ehteshmaul Kabir <61960655+ehteshampantho@users.noreply.github.com>
2024-03-28 16:01:10 +0600

    Time: 937 ms (25.67%), Space: 0B (100.00%) - LeetHub

--- 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
+++ 0181-employees-earning-more-than-their-managers/0181-employees-earning-more-than-their-managers.sql
@@ -1,7 +1,5 @@
 # Write your MySQL query statement below
 
-SELECT m.name AS Employee
-FROM Employee as e
-    JOIN Employee as m
-    ON e.id = m.managerId
-WHERE m.salary > e.salary;    
+SELECT EMP.name AS Employee
+FROM Employee AS EMP, Employee AS MGR
+WHERE EMP.managerID = MGR.id AND EMP.salary > MGR.salary;
